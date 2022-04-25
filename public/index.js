5fcf151e199d90247ec06f4a7af199c9e8ab30b1
Joey <joeyhoellerich@gmail.com>
2022-04-25 14:15:55 -0400

    added server JS

--- public/index.js
+++ public/index.js
@@ -1,9 +1,15 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    // get counter value from server (returns promise object)
+    let serverCount = await fetch("http://192.168.1.158:9001/counter");
+    
+    // grab JSON data from promise object
+    let countJSON = await serverCount.json();
+
+    let countValue = countJSON;
 
     function increment(){
         countValue++;
